be77e02108ef2eb0f5c7b99f31d32ac22671ee48
miPO <kontakt@mipo.pl>
2014-11-20 13:03:34 +0100

    Add a Contact page (solves exercise 3.2)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#Home'
   get 'static_pages/Help'
   get 'static_pages/About'
+  get 'static_pages/Contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get Contact" do
+    get :Contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
 end
