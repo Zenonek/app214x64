41a8286437da96e4e92e5c4590b6f7c81a52a19a
miPO <kontakt@mipo.pl>
2014-11-20 12:56:36 +0100

    Controller test modyfication

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get Home" do
     get :Home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get Help" do
     get :Help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get About" do
     get :About
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
