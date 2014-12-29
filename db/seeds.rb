2aff309bafb505062168d736daddf7003daf33c0
miPO <kontakt@mipo.pl>
2014-12-29 14:09:27 +0100

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,17 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "password",
              password_confirmation: "password",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name:  "Zenon Brzęczyszczykiewicz",
              email: "example@email.com",
              password:              "password",
              password_confirmation: "password",
-             admin: false)
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -25,5 +29,7 @@ User.create!(name:  "Zenon Brzęczyszczykiewicz",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
